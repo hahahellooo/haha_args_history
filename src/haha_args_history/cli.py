83da7a5575ef9047fe75cc6eb52c7994d9c11971
hahahellooo <hahahello777@gmail.com>
2024-07-25 13:49:50 +0900

    return2origin

--- src/haha_args_history/cli.py
+++ src/haha_args_history/cli.py
@@ -1,8 +1,30 @@
+import argparse
+
 def hello_msg():
     return "hello"
+
+#def hi_msg():
+   # return "hi"
 
 def cmd():
     msg = hello_msg()
     print(msg)
 
+    parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
 
+    parser.add_argument('filename') #positional argument         
+    parser.add_argument('-c', '--count') # option that takes a value   
+    parser.add_argument('-v', '--verbose', action='store_true') # on/off flag
+
+
+    args = parser.parse_args()
+    print(args.filename, args.count, args.verbose)
+
+
+    if True:
+        print("verbose ON")
+    else:
+        print("verbose OFF")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,11 @@
-from haha_args_history.cli import hello_msg
+from haha_args_history.cli import hello_msg # ,hi_msg
+#import haha_args_history.cli as cli
 
 def test_hello():
-    m = msg()
-    assert m == "hello2"
+    #m = cli.hello_msg()
+    m = hello_msg()
+    assert m == "hello"
 
+#def test_hi():
+   # m = hi_msg()
+   # assert m == "hi"
